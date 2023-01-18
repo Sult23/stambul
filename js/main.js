30425bcdb8ad395850a28891cb2aac8db57cb213
Sultan <sultan.nurmatov@gmail.com>
2023-01-18 19:55:32 +0600

    new

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.slider__inner').slick({
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	fade: true,
+	speed: 500,
+})
+
+$('.about__slider').slick({
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	fade: true,
+	speed: 500,
+})
+
+var animateLeft = $('.about__left')
+var animateRight = $('.about__right')
+var scrollChange = 800
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop()
+
+	if (scroll >= scrollChange) {
+		animateLeft.addClass('about__left-animation')
+		animateRight.addClass('about__right-animation')
+	}
+})
+
+const tMenu = document.querySelector('.togglemenu>a')
+const navBar = document.querySelector('.list')
+tMenu.addEventListener('click', function () {
+	navBar.classList.toggle('active')
+	tMenu.classList.toggle('active')
+})
